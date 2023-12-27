38b4346965ada4c8c62d3c47a5b62226a76a9205
shubham <shubhamsagar628@gmail.com>
2023-12-27 12:01:10 +0530

    useRef implement

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,41 +1,53 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 const AddTodo = ({onBtnClick}) => {
 
-  const [todoName,setTodoName] = useState()
-  const [dueDate,setDueDate] = useState()
+  // const [todoName,setTodoName] = useState()
+  // const [dueDate,setDueDate] = useState()
+
+  const todoNameElement = useRef()
+  const dueDateElement = useRef()
 
 
-  const handleToDoName = (e) => {
-    setTodoName(e.target.value)
-  }
+  // const handleToDoName = (e) => {
+  //   setTodoName(e.target.value)
+  // }
 
-  const handleDueDate = (e) => {
-    setDueDate(e.target.value)
-  }
+  // const handleDueDate = (e) => {
+  //   setDueDate(e.target.value)
+  // }
 
-  const handleData = () => {
-    onBtnClick(todoName,dueDate)
-    setTodoName("")
-    setDueDate("")
+  const handleData = (e) => {
+    e.preventDefault()
+    const todoName = todoNameElement.current.value
+    const dueDate = dueDateElement.current.value
+    // console.log(todoName)
+   
+    // setTodoName("")
+    // setDueDate("")
+    todoNameElement.current.value =''
+    dueDateElement.current.value=''
+     onBtnClick(todoName,dueDate)
   }
 
     return (
         <div class="container items-container">
+        <form onSubmit={handleData} >
         <div class="row kg-row">
           <div class="col-6">
-            <input type="text" placeholder='Enter Todo Here' onChange={handleToDoName} value={todoName}/>
+            <input type="text" placeholder='Enter Todo Here' ref={todoNameElement}/>
           </div>
           <div class="col-4">
-            <input type="date" onChange={handleDueDate} value={dueDate}/>
+            <input type="date" ref={dueDateElement}/>
           </div>
           <div className="col-2 ">
-       <button type='button' class="btn btn-success kg-button " onClick={handleData}>
+       <button type='submit' class="btn btn-success kg-button " >
             Add 
           </button>
           </div>
      
         </div>
+        </form>
       </div>
     )
 }
